fa695420124962b0bbdf01ea9ce8059a8b0cf81c
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-12-28 12:22:30 -0500

    Delete test_my_module.py

--- test_my_module.py
+++
@@ -1,8 +0,0 @@
-import unittest
-import my_module
-
-class TestMy_module(unittest.TestCase):
-    def test_calculate_sum(self):
-        # TODO: Add appropriate test cases
-        result = my_module.calculate_sum(None)
-        self.assertIsNotNone(result)
